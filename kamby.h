03727d8587a3d9461d7d3b07eb9c7e3af24486c7
Henrique Soares <henriquegogo@gmail.com>
2022-07-30 04:26:38 -0300

    Changing ka_fn signature and return

--- kamby.h
+++ kamby.h
@@ -16,10 +16,11 @@ struct KaNode *ka_set(struct KaNode *node, struct KaNode **env);
 
 struct KaNode *ka_get(struct KaNode *node, struct KaNode **env);
 
+struct KaNode *ka_fn(char *key, struct KaNode *(*fn)(), struct KaNode **env);
+
 struct KaNode *ka_eval(struct KaNode *node, struct KaNode **env);
 
 struct KaNode *ka_parse(char *text);
 
-void ka_fn(char *key, struct KaNode *(*fn)(), struct KaNode **env);
 
 void ka_init(struct KaNode **env);
