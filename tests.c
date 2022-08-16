86eaabe3e5d934398719c452ebe79baaf61bac94
Henrique Gogó <henriquegogo@gmail.com>
2022-08-16 12:25:07 -0300

    Tests for stack and context call

--- tests.c
+++ tests.c
@@ -65,6 +65,52 @@ void test_definitions() {
   assert(ka_get(ka_idf("number"), &env)->num == 789);
 }
 
+void test_stack() {
+  printf("- Getting data from stack\n");
+  struct KaNode *pos = malloc(KANODE_SIZE);
+  struct KaNode *env = ka_init();
+  ka_def(ka_link( ka_str("num1"), ka_num(987), 0), &env);
+  ka_def(ka_link( ka_str("num2"), ka_num(789), 0), &env);
+  assert(ka_eval(ka_parser(".", &pos), &env)->num == 789);
+  pos = malloc(KANODE_SIZE);
+  assert(ka_eval(ka_parser(". 1", &pos), &env)->num == 789);
+  pos = malloc(KANODE_SIZE);
+  assert(ka_eval(ka_parser(". 2", &pos), &env)->num == 987);
+  pos = malloc(KANODE_SIZE);
+  assert(!ka_eval(ka_parser(". 3", &pos), &env)->num);
+}
+
+void test_call() {
+  printf("- Call block passing context\n");
+  struct KaNode *pos = malloc(KANODE_SIZE);
+  struct KaNode *env = ka_init();
+  ka_def(ka_link( ka_str("num1"), ka_num(987), 0), &env);
+  ka_eval(ka_parser("list = [12 23 34]", &pos), &env);
+  pos = malloc(KANODE_SIZE);
+  ka_eval(ka_parser("obj = [name := 'me'; age := 20]", &pos), &env);
+  ka_def(ka_link( ka_str("num2"), ka_num(789), 0), &env);
+  pos = malloc(KANODE_SIZE);
+  assert(ka_eval(ka_parser("list :: {.}", &pos), &env)->num == 12);
+  pos = malloc(KANODE_SIZE);
+  assert(ka_eval(ka_parser("list :: {. 1}", &pos), &env)->num == 12);
+  pos = malloc(KANODE_SIZE);
+  assert(ka_eval(ka_parser("list :: {. 2}", &pos), &env)->num == 23);
+  pos = malloc(KANODE_SIZE);
+  assert(ka_eval(ka_parser("list :: {. 3}", &pos), &env)->num == 34);
+  pos = malloc(KANODE_SIZE);
+  assert(!ka_eval(ka_parser("list :: {. 4}", &pos), &env)->num);
+  pos = malloc(KANODE_SIZE);
+  assert(ka_eval(ka_parser("obj :: {age}", &pos), &env)->num == 20);
+  pos = malloc(KANODE_SIZE);
+  ka_eval(ka_parser("obj :: {age = 40}", &pos), &env);
+  pos = malloc(KANODE_SIZE);
+  assert(ka_eval(ka_parser("obj :: {age}", &pos), &env)->num == 40);
+  pos = malloc(KANODE_SIZE);
+  assert(ka_eval(ka_parser(". 1", &pos), &env)->num == 789);
+  pos = malloc(KANODE_SIZE);
+  assert(ka_eval(ka_parser(". 4", &pos), &env)->num == 987);
+}
+
 int main() {
   printf("TESTING...\n");
 
@@ -73,6 +119,8 @@ int main() {
   test_evaluation();
   test_constructors();
   test_definitions();
+  test_stack();
+  test_call();
 
   printf("ALL TESTS PASSED!\n");
 
