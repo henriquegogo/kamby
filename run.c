2a809eadd3b45ca811b40101f0de6df93cc121ec
Henrique Soares <henriquegogo@gmail.com>
2023-11-20 23:49:41 -0300

    Constructor and destructor

--- run.c
+++ run.c
@@ -6,14 +6,20 @@
 
 char *ka_run(char *text) {
   struct KaNode *env = ka_init();
-  struct KaNode *pos = calloc(1, KANODE_SIZE);
+  struct KaNode *pos = ka_new();
   struct KaNode *res = ka_eval(ka_parser(text, &pos), &env);
-  while (res->next) res = res->next;
+  while (res->next) {
+    struct KaNode *prev = res;
+    res = res->next;
+    free(prev);
+  }
   char *output = calloc(1, sizeof(long long));
   if (res->type == KA_NUM) {
     sprintf(output, "%lld", res->num);
   } else if (res->type == KA_STR) {
     strcpy(output, res->str);
   }
+  ka_free(&env);
+  ka_free(&pos);
   return output;
 }
