ff19f2bb666ad897579575d2848fb81628028302
Henrique Soares <henriquegogo@gmail.com>
2025-01-03 17:57:18 -0300

    Improve copy/memory tests

--- tests.c
+++ tests.c
@@ -77,22 +77,27 @@ void test_list() {
 }
 
 void test_copy() {
-  KaNode *list = ka_list(ka_string("Hello"), ka_string("World"), NULL);
-  KaNode *child = list->children;
+  KaNode *list = ka_list(ka_number(1), ka_number(2), ka_number(3), NULL);
+  KaNode *first = list->children;
   KaNode *list_copy = ka_copy(list);
-  KaNode *child_copy = ka_copy(child);
+  KaNode *first_copy = ka_copy(first);
+  KaNode *second_copy = ka_copy(list->children->next);
+  KaNode *third_copy = ka_copy(list->children->next->next);
 
-  assert(child_copy->type == child->type && child->type == KA_STRING);
-  assert(strcmp(child_copy->string, child->string) == 0);
-  assert(*child_copy->refcount == *child->refcount && *child->refcount == 0);
-  assert(child->next != NULL && child_copy->next == NULL);
+  assert(first_copy->type == first->type && first->type == KA_NUMBER);
+  assert(strcmp(first_copy->string, first->string) == 0);
+  assert(*first_copy->refcount == *first->refcount && *first->refcount == 0);
+  assert(first->next != NULL && first_copy->next == NULL);
+  assert(!second_copy->next && !third_copy->next);
 
   assert(list_copy->type == list->type && list->type == KA_LIST);
   assert(*list_copy->refcount == *list->refcount && *list->refcount == 1);
   ka_free(list_copy);
   assert(*list->refcount == 0);
 
-  ka_free(child_copy);
+  ka_free(third_copy);
+  ka_free(second_copy);
+  ka_free(first_copy);
   ka_free(list);
 }
 
