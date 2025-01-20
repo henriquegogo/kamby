977e0df6b46c5e7e0bc8b00d1c13d50ba1db81b2
Henrique Soares <henriquegogo@gmail.com>
2025-01-19 22:48:38 -0300

    Test for ka_func

--- tests.c
+++ tests.c
@@ -69,6 +69,43 @@ void test_symbol() {
   ka_free(node);
 }
 
+void test_func() {
+  KaNode *node = ka_func(ka_def);
+
+  assert(node->type == KA_FUNC);
+  assert(node->key == NULL);
+  assert(node->func == ka_def);
+  assert(*node->refcount == 0);
+  assert(node->next == NULL);
+
+  ka_free(node);
+}
+
+void test_copy() {
+  KaNode *list = ka_list(ka_number(1), ka_number(2), ka_number(3), NULL);
+  KaNode *first = list->children;
+  KaNode *list_copy = ka_copy(list);
+  KaNode *first_copy = ka_copy(first);
+  KaNode *second_copy = ka_copy(list->children->next);
+  KaNode *third_copy = ka_copy(list->children->next->next);
+
+  assert(first_copy->type == first->type && first->type == KA_NUMBER);
+  assert(!strcmp(first_copy->string, first->string));
+  assert(*first_copy->refcount == *first->refcount && *first->refcount == 0);
+  assert(first->next != NULL && first_copy->next == NULL);
+  assert(!second_copy->next && !third_copy->next);
+
+  assert(list_copy->type == list->type && list->type == KA_LIST);
+  assert(*list_copy->refcount == *list->refcount && *list->refcount == 1);
+  ka_free(list_copy);
+  assert(*list->refcount == 0);
+
+  ka_free(third_copy);
+  ka_free(second_copy);
+  ka_free(first_copy);
+  ka_free(list);
+}
+
 void test_list() {
   KaNode *node = ka_list(ka_number(42), ka_string("Hello"), NULL);
 
@@ -117,31 +154,6 @@ void test_block() {
   assert(node->next == NULL);
 
   ka_free(node);
-}
-
-void test_copy() {
-  KaNode *list = ka_list(ka_number(1), ka_number(2), ka_number(3), NULL);
-  KaNode *first = list->children;
-  KaNode *list_copy = ka_copy(list);
-  KaNode *first_copy = ka_copy(first);
-  KaNode *second_copy = ka_copy(list->children->next);
-  KaNode *third_copy = ka_copy(list->children->next->next);
-
-  assert(first_copy->type == first->type && first->type == KA_NUMBER);
-  assert(!strcmp(first_copy->string, first->string));
-  assert(*first_copy->refcount == *first->refcount && *first->refcount == 0);
-  assert(first->next != NULL && first_copy->next == NULL);
-  assert(!second_copy->next && !third_copy->next);
-
-  assert(list_copy->type == list->type && list->type == KA_LIST);
-  assert(*list_copy->refcount == *list->refcount && *list->refcount == 1);
-  ka_free(list_copy);
-  assert(*list->refcount == 0);
-
-  ka_free(third_copy);
-  ka_free(second_copy);
-  ka_free(first_copy);
-  ka_free(list);
 }
 
 void test_get() {
@@ -212,7 +224,7 @@ int main() {
   test_number();
   test_string();
   test_symbol();
-  //test_func();
+  test_func();
   test_copy();
   test_list();
   test_expr();
