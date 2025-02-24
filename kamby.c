2bb214baa6a89eacef2fd8271de63d73ac6214d1
Henrique Soares <henriquegogo@gmail.com>
2025-02-23 21:58:58 -0300

    Small REPL improvements

--- kamby.c
+++ kamby.c
@@ -3,7 +3,15 @@
 #include "kamby.h"
 
 KaNode *print(KaNode **ctx, KaNode *args) {
-  printf("%s\n", args->string);
+  switch (args->type) {
+    case KA_NUMBER:
+      printf("%.2Lf\n", *args->number);
+      break;
+    case KA_STRING:
+      printf("%s\n", args->string);
+      break;
+    default:;
+  }
   ka_free(args);
   return NULL;
 }
@@ -16,12 +24,17 @@ int main() {
   ka_free(ka_def(&ctx, ka_chain(ka_symbol("def"), ka_func(ka_def), NULL)));
   ka_free(ka_def(&ctx, ka_chain(ka_symbol("print"), ka_func(print), NULL)));
 
+  printf("Valid keywords: def, print\n");
+  printf("Usage:\n");
+  printf("  def <symbol> <number|string>\n");
+  printf("  print <symbol>\n\n");
+
   while (1) {
     printf("kamby> ");
     fflush(stdout);
     fgets(input, 1024, stdin);
     KaNode *expr = ka_parser(input, (pos = 0, &pos));
-    KaNode *result = ka_eval(&ctx, expr);
+    KaNode *result = ka_eval(&ctx, expr->children);
     ka_free(result), ka_free(expr);
     input[0] = '\0';
   }
