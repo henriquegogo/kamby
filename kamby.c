e10651a2042da547fab02413b50be46570eb1051
Henrique Soares <henriquegogo@gmail.com>
2025-05-13 17:14:02 -0300

    REPL read file content by pipe and stdin

--- kamby.c
+++ kamby.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "kamby.h"
 
 int main() {
@@ -7,26 +8,29 @@ int main() {
   int pos = 0;
   char input[8192];
 
-  printf("Valid keywords:\n  ");
-  int cols = 0;
-  for (KaNode *curr = ctx->next; curr->key && curr->next; curr = curr->next) {
-    if (strlen(curr->key) > 0) printf("%s ", curr->key);
-    if ((cols += strlen(curr->key) + 1) > 50) { printf("\n  "); cols = 0; }
-  }
-  printf("\n\n");
-
-  printf("Usage:\n");
-  printf("  <symbol> = <number|string>\n");
-  printf("  print <symbol>\n\n");
-
-  while (1) {
+  if (isatty(fileno(stdin))) {
+    printf("Valid keywords:\n  ");
+    int cols = 0;
+    for (KaNode *curr = ctx->next; curr->key && curr->next; curr = curr->next) {
+      if (strlen(curr->key) > 0) printf("%s ", curr->key);
+      if ((cols += strlen(curr->key) + 1) > 50) { printf("\n  "); cols = 0; }
+    }
+    printf("\n\n");
+    printf("Usage:\n");
+    printf("  <symbol> = <number|string>\n");
+    printf("  print <symbol>\n\n");
     printf("kamby> ");
-    fflush(stdout);
-    fgets(input, 8192, stdin);
+  }
+
+  while (fgets(input, 8192, stdin)) {
     KaNode *expr = ka_parser(input, (pos = 0, &pos));
     KaNode *result = ka_eval(&ctx, expr);
     ka_free(result), ka_free(expr);
     input[0] = '\0';
+    if (isatty(fileno(stdin))) {
+      printf("kamby> ");
+      fflush(stdout);
+    }
   }
 
   ka_free(ka_del(&ctx, ka_symbol("ctx")));
